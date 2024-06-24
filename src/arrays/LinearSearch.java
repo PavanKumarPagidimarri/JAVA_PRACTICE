57e6161309f3158e7d24b0e8518e1281bc9d3b37
pagidimarripavankumar <pavanpagidimarri5@gmail.com>
2024-06-24 08:42:30 +0100

    count the elements

--- src/arrays/LinearSearch.java
+++ src/arrays/LinearSearch.java
@@ -6,21 +6,25 @@ public class LinearSearch {
 
 
     public static void main(String[] args) {
-        int age[] = {10,20,17,20,22,10};
+        int age[] = {10,20,17,20,22,10,10, 10};
         Scanner scanobj = new Scanner(System.in);
         System.out.println("Enter search element:");
         int search_number = scanobj.nextInt();
-
+        int count = 0;
         boolean status = false;
         for (int j : age) {
             if (search_number == j) {
-                System.out.println(j + " Number Found in the array");
                 status = true;
-                break;
+                count++;
             }
+        }
+        if (status) {
+            System.out.println(search_number + " Number Found in the array");
+            System.out.println(search_number + " Appeared " + " " + count + " Times");
         }
         if(!status){
             System.out.println("Element not found in the array");
         }
+
     }
 }
