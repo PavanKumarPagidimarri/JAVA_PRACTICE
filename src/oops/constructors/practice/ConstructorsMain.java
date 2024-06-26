e7a88d3d1714525393d46a000b239f8ac9faed81
pagidimarripavankumar <pavanpagidimarri5@gmail.com>
2024-06-26 12:07:07 +0100

    Adding comments

--- src/oops/constructors/practice/ConstructorsMain.java
+++ src/oops/constructors/practice/ConstructorsMain.java
@@ -3,10 +3,12 @@ package oops.constructors.practice;
 public class ConstructorsMain {
 
     public static void main(String[] args) {
+        //calling default constructor
         Constructors defcon = new Constructors();
         int sum =defcon.sum();
         System.out.println("Sum calculated by default constructor : "+sum);
 
+        //calling parameterized constructor
         Constructors parameter_con = new Constructors(12, 13);
         int sum_by_parameters = parameter_con.sum();
         System.out.println("Sum calculated by parameter constructor : "+sum_by_parameters);
