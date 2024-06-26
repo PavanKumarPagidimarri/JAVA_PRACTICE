00fd9abd54fa1d7838972ab57d1a154bb6824ad6
pagidimarripavankumar <pavanpagidimarri5@gmail.com>
2024-06-26 21:49:34 +0100

    Added Pattern programs

---
+++ src/patterns/Patterns.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class Patterns {
+  void triangle() {
+      for (int i = 0; i <= 5; i++) {
+          for (int j=0;j<=i;j++) {
+              System.out.print("* ");
+          }
+          System.out.println(" ");
+      }
+  }
+
+   void reverse_triangle(){
+
+  }
+
+
+
+}
---
+++ src/patterns/PatternsMain.java
@@ -0,0 +1,9 @@
+package patterns;
+
+public class PatternsMain {
+    public static void main(String[] args) {
+       Patterns obj = new Patterns();
+        System.out.println("This is triangle method output");
+        obj.triangle();
+    }
+}
